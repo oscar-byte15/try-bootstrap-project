5dd2848b604bfaabb0a9432dc7503cf5ca5a4c2c
Oscar <54118125+oscar-byte15@users.noreply.github.com>
2023-03-09 13:54:08 -0500

    Fixing eslint error
    Fixing eslint error

--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
-const port = document.getElementById('portfolio');
-const about = document.getElementById('about');
-const contact = document.getElementById('contact');
-let cont = 0;
+window.onload = () => {
+  const port = document.getElementById('portfolio');
+  const about = document.getElementById('about');
+  const contact = document.getElementById('contact');
+  let cont = 0;
 
-function cambio() {
-  if (cont === 0) {
-    document.getElementById('cont-menu').style.transform = 'translateX(-100%)';
-    document.getElementById('cont-menu').style.position = 'relative';
-    document.getElementById('btn-menu').style.position = 'absolute';
-    // document.getElementById('cont-menu').style.= '1';
-    cont = 1;
-  } else {
-    document.getElementById('cont-menu').style.visibility = 'visible';
-    document.getElementById('cont-menu').style.width = '100%';
-    document.getElementById('btn-menu').style.position = 'absolute';
-  }
+  function cambio() {
+    if (cont === 0) {
+      document.getElementById('cont-menu').style.transform = 'translateX(-100%)';
+      document.getElementById('cont-menu').style.position = 'relative';
+      document.getElementById('btn-menu').style.position = 'absolute';
+      // document.getElementById('cont-menu').style.= '1';
+      cont = 1;
+    } else {
+      document.getElementById('cont-menu').style.visibility = 'visible';
+      document.getElementById('cont-menu').style.width = '100%';
+      document.getElementById('btn-menu').style.position = 'absolute';
+    }
   // document.getElementById('btn-menu').style.checked= "hidden";
-}
+  }
 
-port.addEventListener('click', cambio, true);
-about.addEventListener('click', cambio, true);
-contact.addEventListener('click', cambio, true);
+  port.addEventListener('click', cambio, true);
+  about.addEventListener('click', cambio, true);
+  contact.addEventListener('click', cambio, true);
+};
