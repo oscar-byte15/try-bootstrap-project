51fe6a9460d31f8a6086cb9fcece43ac77f416b4
Oscar <54118125+oscar-byte15@users.noreply.github.com>
2023-03-09 11:47:20 -0500

    Fixing linter errors
    Fixing linter errors

--- src/main.js
+++ src/main.js
@@ -1,27 +1,23 @@
 const port = document.getElementById('portfolio');
 const about = document.getElementById('about');
 const contact = document.getElementById('contact');
-let cont =0;
+let cont = 0;
 
- function cambio(){
-    if( cont==0) {
-   
-        document.getElementById('cont-menu').style.transform= 'translateX(-100%)';
-        document.getElementById('cont-menu').style.position= 'relative';
-        document.getElementById('btn-menu').style.position= 'absolute';
-        //document.getElementById('cont-menu').style.= '1';
-       cont=1;
-    }
-    else{
-        document.getElementById('cont-menu').style.visibility= 'visible';
-        document.getElementById('cont-menu').style.width= '100%';
-        document.getElementById('btn-menu').style.position= 'absolute';
-    }
-       // document.getElementById('btn-menu').style.checked= "hidden";
-   
-  
+function cambio() {
+  if (cont === 0) {
+    document.getElementById('cont-menu').style.transform = 'translateX(-100%)';
+    document.getElementById('cont-menu').style.position = 'relative';
+    document.getElementById('btn-menu').style.position = 'absolute';
+    // document.getElementById('cont-menu').style.= '1';
+    cont = 1;
+  } else {
+    document.getElementById('cont-menu').style.visibility = 'visible';
+    document.getElementById('cont-menu').style.width = '100%';
+    document.getElementById('btn-menu').style.position = 'absolute';
+  }
+  // document.getElementById('btn-menu').style.checked= "hidden";
 }
 
-port.addEventListener('click',cambio,true);
-about.addEventListener('click',cambio,true);
-contact.addEventListener('click',cambio,true);
+port.addEventListener('click', cambio, true);
+about.addEventListener('click', cambio, true);
+contact.addEventListener('click', cambio, true);
